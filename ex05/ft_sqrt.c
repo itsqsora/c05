bb34938fb03da4208cc098b04bacfd55bdb9d269
Mac <mac@Mac-2.local>
2022-08-01 12:29:33 +0700

    complete

--- ex05/ft_sqrt.c
+++ ex05/ft_sqrt.c
@@ -6,7 +6,7 @@
 /*   By: mac <sorungta@42bangkok.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 07:11:48 by mac               #+#    #+#             */
-/*   Updated: 2022/08/01 10:49:23 by mac              ###   ########.fr       */
+/*   Updated: 2022/08/01 12:29:09 by mac              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,29 @@
 
 int	ft_sqrt(int nb)
 {
-	int	i;
-	int	sq;
+	long long	i;
 
+	if (nb <= 0)
+		return (0);
 	i = 1;
-	sq = 1;
-	if (nb == 0)
-		return (0);
 	while (i * i < nb)
-	{
-		i++;
-	}
-	if ((nb % i) == 0)
+		++i;
+	if (i * i == nb)
 		return (i);
-	else
-		return (0);
+	return (0);
 }
 /*
 int	main(void)
 {
-    printf("%d", ft_sqrt(650));
+	printf("%d\n", ft_sqrt(268435456));
+	printf("%d\n", ft_sqrt(134167199));
+	printf("%d\n", ft_sqrt(90935296));
+	printf("%d\n", ft_sqrt(92260));
+	printf("%d\n", ft_sqrt(716632900));
+	printf("%d\n", ft_sqrt(62904750));
+	printf("%d\n", ft_sqrt(1545040249));
+	printf("%d\n", ft_sqrt(70765342));
+	printf("%d\n", ft_sqrt(68807025));
+	printf("%d\n", ft_sqrt(46629167));
+	return (0);
 }*/
